5bcc4b979976317f5cbfcd7b31e895756b225c81
Yung Dai <yungchidai@gmail.com>
2014-11-14 12:55:26 -0500

    Merged changes

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -3,9 +3,16 @@ class Admin::AdminController < ApplicationController
 
   end
 
-end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_user_url
+  end
+
 
 private
-def user_params
-  params.require(:user).permit(:email, :password, :password_confirmation, :name)
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
+  end
 end
+
