0360412445de35869f71defa44df3ce851a42638
Yung Dai <yungchidai@gmail.com>
2014-11-13 22:37:24 -0500

    synching the admin/admin/index.html.erb file

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,7 +7,8 @@ class Restaurant < ActiveRecord::Base
   validates_presence_of :name, :capacity, :phone_number
   validates_presence_of :street_number, :street_name, :city, :province, :postal_code
   validates :capacity, :numericality => {greater_than: 0}
-  validates_format_of :phone_number, :with => /\d{3}[ -]?\d{3}[ -]?\d{4}/, :message => 'Phone number must be in the format XXX-XXX-XXXX. Dashes/spaces optional'
+  validates_format_of :phone_number, :with => /\d{3}[ .-]?\d{3}[ .-]?\d{4}/, :message => 'Phone number must be in the
+ format XXX-XXX-XXXX. Dashes/spaces/dots optional'
 
   # TODO Validate that the hours do not overlap.
   # TODO Implement hours as input string.
